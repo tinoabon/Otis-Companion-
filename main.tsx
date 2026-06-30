c48bf5b1cc004dc6d6eeb03e9f9d42385bf80ca3
tinoabon <tabon.dc@gmail.com>
2026-06-30 06:31:02 -0700

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './otis-phase1.tsx'
+import App from './App.tsx'
 import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
